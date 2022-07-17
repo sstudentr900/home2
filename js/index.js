4bfe871fa84e956e4cbede271b4f0a4898f301a1
John Doe <johndoe@example.com>
2022-07-17 22:59:11 +0800

    update

--- js/index.js
+++ js/index.js
@@ -45,7 +45,7 @@ function creatMainHtml(url,obj){
         if(response.ok) return response.text();
     }).then(function(oldText) {
         let text = oldText;
-        console.log(text.includes('<body id="program">'))
+        // console.log(text.includes('<body id="program">'))
         if(text.includes('<body id="program">')){
             text = oldText.split('<body id="program">')[1].split("</body>")[0];
         }
