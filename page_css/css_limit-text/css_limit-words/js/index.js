9f87121ac19f45b3ebd0777d890ba2e88352797c
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2022-07-18 16:25:34 +0800

    1

--- page_css/css_limit-text/css_limit-words/js/index.js
+++
@@ -1,6 +0,0 @@
-// var p=document.getElementsByTagName('p'),pl=p.length;
-// while(pl--){
-//  lineheight=parseInt(window.getComputedStyle(p[pl]).getPropertyValue("line-height"));
-//  var lines=Math.floor(10/lineheight);
-//  p[pl].style['-webkit-line-clamp']=lines;
-// }
