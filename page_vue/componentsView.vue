e62cb5948c87daf8096c171ba2da6f970f17f8c4
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2022-08-18 14:26:52 +0800

    1

--- page_vue/componentsView.vue
+++ page_vue/componentsView.vue
@@ -1,18 +1,5 @@
 <template>
     <div>
-        <h2>components</h2>
-        <h4>註冊全局組件</h4>
-        <pre>
-            //main.js
-            import fileName from fileUrl
-            //第一個參數:全局組件名稱,第二個參數:組件
-            vue.component(fileName.name,fileName)
-
-            //template
-            //直接打名稱即可調用
-            fileName
-        </pre>
-        <br>
         <h2>全局引用(globalProperties)</h2>
         <pre>
             //main.js
@@ -28,7 +15,7 @@
         </pre>
         <button @click="getData">getData</button>
         <br>
-        <h2>全局引用(provide/inject)</h2>
+        <h2>global variable in Vue(provide/inject)</h2>
         <pre>
             //main.js
             import * as api from '@/api'; //全域API
@@ -47,9 +34,15 @@
         <br>
         <br>
         <hr>
-        <a href="https://www.youtube.com/watch?v=3pDNY4fLZGc&list=PLmOn9nNkQxJEFpabd412vGw_k7-lHlJOP&index=13">全局组件</a>
-        <a href="https://stackoverflow.com/questions/65184107/how-to-use-vue-prototype-or-global-variable-in-vue-3">全域配置</a>
-        <a href="https://v3.cn.vuejs.org/guide/migration/global-api.html#vue-prototype-%E6%9B%BF%E6%8D%A2%E4%B8%BA-config-globalproperties">全局 API</a>
+        <h4>參考</h4>
+        <ul>
+            <li>
+            <a href="https://stackoverflow.com/questions/65184107/how-to-use-vue-prototype-or-global-variable-in-vue-3" target="_blank">global variable</a>
+            </li>
+            <li>
+            <a href="https://www.youtube.com/watch?v=3pDNY4fLZGc&list=PLmOn9nNkQxJEFpabd412vGw_k7-lHlJOP&index=13" target="_blank">註冊全局組件</a>
+            </li>
+        </ul>
     </div>
 </template>
 <script>
