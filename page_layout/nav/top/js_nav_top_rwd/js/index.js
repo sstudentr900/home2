5b0c493a23844e72e01a6ffba33354b420b8ae91
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2023-03-03 13:57:11 +0800

    1

--- page_layout/nav/top/js_nav_top_rwd/js/index.js
+++
@@ -1,9 +0,0 @@
-function navshow(){
-    var navul = document.getElementById('navul');
-    if(!navul) return;
-    if(navul.offsetHeight){
-        navul.className='list-inline';
-    }else{
-        navul.className='list-inline active';
-    }
-}
--- page_layout/nav/top/nav-fixed/js_scroll-fixed/js/index.js
+++
@@ -1,7 +0,0 @@
-window.onresize =window.onload = window.onscroll = function(){
-    var odiv = document.getElementById('div1');
-    var scrolltop=document.documentElement.scrollTop||document.body.scrollTop;
-    var t =(document.documentElement.clientHeight-odiv.offsetHeight)/2;
-    var k = scrolltop+t;
-   odiv.style.top=scrolltop+t+'px';
-}
